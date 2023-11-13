90a491677e0b88708e6a0e8a0e70557809e72ec3
vegardbv <vegard.b.vognstolen@gmail.com>
2023-11-13 16:40:57 +0100

    #9 design frontpage

--- Web-Client/src/pages/HomePage.js
+++ Web-Client/src/pages/HomePage.js
@@ -1,31 +1,81 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { Counter } from "../features/counter/counter";
 import "../css/HomePage.css";
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [screenSize, setScreenSize] = useState(getCurrentDimension());
+  const [skyScraperHeight, setSkyScraperHeight] = useState(1800);
+  const [garbageHeight, setGarbageHeight] = useState(250);
+
+  function getCurrentDimension() {
+    return {
+      innerWidth: window.innerWidth,
+      innerHeight: window.innerHeight,
+      outerHeight: window.outerHeight,
+      screenX: window.screenX,
+      screenY: window.screenY,
+      scrollY: window.scrollY,
+      height: window.document.body.offsetHeight,
+    };
+  }
+
+  useEffect(() => {
+    const updateDimension = () => {
+      setScreenSize(getCurrentDimension());
+    };
+    window.addEventListener("scroll", updateDimension);
+
+    if (
+      screenSize.scrollY / (screenSize.height - screenSize.innerHeight) <
+      0.6
+    ) {
+      setSkyScraperHeight((1800 * screenSize.scrollY) / 700);
+    }
+    if (
+      screenSize.scrollY / (screenSize.height - screenSize.innerHeight) <
+      0.9
+    ) {
+      setGarbageHeight((250 * screenSize.scrollY) / 300);
+    }
+
+    if (
+      screenSize.scrollY / (screenSize.height - screenSize.innerHeight) ===
+      1
+    ) {
+    }
+
+    return () => {
+      window.removeEventListener("scroll", updateDimension);
+    };
+  }, [screenSize]);
+
   return (
-    <div className="homePageWrapper">
+    <>
       <Navbar />
 
       <div className="homePageContentWrapper">
         <img
           id="daybackground"
-          src={require("../images/daybackground.jpg")}
+          src={require("../images/dayBackground.png")}
           alt="background"
         />
         <img
           id="garabgeDumpTransparent"
-          src={require("../images/garabgeDumpTransparent.jpg")}
+          src={require("../images/garabgeDumpTransparent.png")}
           alt="background"
+          style={{ height: garbageHeight }}
         />
         <img
           id="sckyscrapersWOBackground"
-          src={require("../images/sckyscrapersWOBackground.jpg")}
+          src={require("../images/sckyscrapersWOBackground.png")}
           alt="background"
+          style={{ height: skyScraperHeight }}
         />
       </div>
-    </div>
+
+      <div id="textDisplay">Clean Thy Shiet</div>
+    </>
   );
 };
 
